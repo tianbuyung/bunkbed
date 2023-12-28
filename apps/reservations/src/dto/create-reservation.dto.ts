9c0b2f8a26defb3710b6ee30a66a70f7117dfbe4
Septian Maulana <septiandev028@gmail.com>
2023-12-28 22:15:34 +0700

    feat: add validation pipe into create reservation dto
    feat: set whitelist true on main.ts app/reservations for validation pipe

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,7 +1,20 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
-  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
