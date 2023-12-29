a2dbe9e5d4130ef93f197609b5e3e2a8f6b79bfc
Septian Maulana <septiandev028@gmail.com>
2023-12-29 08:31:02 +0700

    build: integrate database module with reservations module

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { DatabaseModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/user.schema';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
