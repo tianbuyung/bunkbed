7c32ab314e14a0a3e5aa844cc6d99b82d6a7b302
Septian Maulana <septiandev028@gmail.com>
2023-12-30 23:52:20 +0700

    feat: create local auth guard extend from passport
    feat: create current user decorator to define user context
    
    feat: define login in auth controller

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { UserDocument } from './users/models/user.schema';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
+  }
 
   @Get()
   getHello(): string {
