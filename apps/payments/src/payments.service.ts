767206f1c4f1d3ae8ab19baa4cba948469b9d724
Septian Maulana <septiandev028@gmail.com>
2024-01-04 00:15:49 +0700

    fix: fix stripe new api requirement issue

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -15,18 +15,23 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
+  async createCharge({ amount }: CreateChargeDto) {
+    // const paymentMethod = await this.stripe.paymentMethods.create({
+    //   type: 'card',
+    //   card,
+    // });
 
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
-      amount: amount * 100,
+      // payment_method: paymentMethod.id,
+      payment_method: 'pm_card_visa', // change this follow new stripe api requirement
+      amount: Math.round(amount * 100),
       confirm: true,
-      payment_method_types: ['card'],
+      // payment_method_types: ['card'],
       currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
     });
 
     return paymentIntent;
