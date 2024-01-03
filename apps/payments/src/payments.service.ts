f1b925e144206c02c59882f910a0fb00faf57434
Septian Maulana <septiandev028@gmail.com>
2024-01-03 12:48:41 +0700

    feat: define create charge method in payments service

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -13,7 +13,23 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  getHello(): string {
-    return 'Hello World!';
+  async createCharge(
+    card: Stripe.PaymentMethodCreateParams.Card1,
+    amount: number,
+  ) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
   }
 }
