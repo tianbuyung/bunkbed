e396b2d1bbc17d62924f87afbff78381e5909355
Septian Maulana <septiandev028@gmail.com>
2024-01-05 19:53:14 +0700

    feat: create event pattern for notifications controller to receive notify by email
    feat: define notify email dto

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
 import { NotificationsService } from './notifications.service';
+import { NotifyEmailDto } from './dto/notify-email.dto';
 
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.notificationsService.getHello();
+  @UsePipes(new ValidationPipe())
+  @EventPattern('notify_email')
+  async notifyEmail(@Payload() data: NotifyEmailDto) {
+    this.notificationsService.notifyEmail(data);
   }
 }
