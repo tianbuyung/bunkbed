15c62c7c6b39706966441cf83a47be71bb69f422
Septian Maulana <septiandev028@gmail.com>
2024-01-05 20:19:28 +0700

    feat: integrate notifications service into payments service
    chore: update reservations controller and service to add email as argument to send into payments service
    
    feat: define payments create charge dto

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import { ClientProxy } from '@nestjs/microservices';
 
-import { CreateChargeDto } from '@app/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -13,9 +15,13 @@ export class PaymentsService {
     },
   );
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
     // const paymentMethod = await this.stripe.paymentMethods.create({
     //   type: 'card',
     //   card,
@@ -33,6 +39,8 @@ export class PaymentsService {
         allow_redirects: 'never',
       },
     });
+
+    this.notificationsService.emit('notify_email', { email });
 
     return paymentIntent;
   }
