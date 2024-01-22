a84e6ebcb41f5ded65783b2d0bf0662ec1902876
Septian Maulana <septiandev028@gmail.com>
2024-01-22 15:43:50 +0700

    refactor: refactor user schema into common libraries
    chore: update dockerfile to copy only file inside the service with dependency
    
    fix: delete unnecessary file

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,8 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
-import { AbstractRepository } from '@app/common';
-import { UserDocument } from './models/user.schema';
+import { AbstractRepository, UserDocument } from '@app/common';
 import { Model } from 'mongoose';
 
 @Injectable()
